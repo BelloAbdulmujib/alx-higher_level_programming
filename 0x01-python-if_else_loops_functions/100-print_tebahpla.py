f7e8d4ebba74d6b5cbfd4a7daa02091a5835709a
AbdulmujibBello <belloabdulmujib3@gmail.com>
2023-11-29 15:41:09 -0800

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
- for char i in range(90, 64, -1):
-    print("{:c}".format(char(i + 32 * (i % 2))), end="")
+
+for i in range(122, 96, -1):
+
+    if i % 2 != 0:
+
+        i = i - 32
+
+    print("{}".format(chr(i)), end="")
