35f0b3e629d964c52380b15627e6dd76fe635144
AbdulmujibBello <belloabdulmujib3@gmail.com>
2023-11-29 13:59:30 -0800

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,8 @@
-#!/usr/bin/python3
- for i in range(90, 64, -1):
-    print("{:c}".format(i + 32 * (i % 2)), end="")
+#!/usr/bin/env python3
+
+for i in range(90, 64, -1):
+    if i % 2 == 0:
+        print(chr(i), end="")
+    else:
+        print(chr(i + 32), end="")
+
