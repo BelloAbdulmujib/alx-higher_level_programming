467d84511f2e6658791ee456af5b9997305c6c14
AbdulmujibBello <belloabdulmujib3@gmail.com>
2023-11-30 15:00:27 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-"""Program that imports functions from the file calculator_1.py, does some Maths, and prints the result"""
-
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
