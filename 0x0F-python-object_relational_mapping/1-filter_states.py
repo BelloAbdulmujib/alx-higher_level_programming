fd14f2f86abdd9149fae115ed75e2963b8399d6c
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-03-17 03:30:13 -0700

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = dbi.cursor()
-    cur.execute("SELECT * FROM states starting with "N" ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE id = %s", STARTING with "N")
     query_rows = cur.fetchall()
     for row in query_rows:
     print(row)
