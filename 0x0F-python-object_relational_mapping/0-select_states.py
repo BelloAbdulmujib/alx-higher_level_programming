2a0dd8910babdb1904df10cfbaded0f07299aa90
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-03-16 17:10:49 -0700

    0-select_states.p

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,4 +2,14 @@
 -- create state table in hbtn_0e_0_usa with data
 CREAT DATABASE IF EXIST hbtn_0e_0_usa;
 import MySQLdb
-db = MySQLdb.connect(host="MY_HOST", port="3360", user="my_user", passwd="MY_PASSW", db="MY_DB")
+import sys
+
+if __name__ == __"main"__
+db = MySQLdb.connect(host="localhost", port=3360, user=sys.argv[1], passwd=argv[2], db=argv[3])
+cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
