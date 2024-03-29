250548dea683a7708c15b10b615156e355e39f95
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-03-28 21:32:06 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,6 +2,9 @@
 """ A script that fetches URL of a website
 """
 import urllib.request
-req = urllib.request.Request('https://alx-intranet.hbtn.io/status/')
-with urllib.request.urlopen(req) as response:
-    the_page = response.read()
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf-8')))
