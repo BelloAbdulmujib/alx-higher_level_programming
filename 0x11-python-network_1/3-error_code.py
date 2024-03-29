f4e9df516121bfe888c7442e6413380720134770
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-03-28 21:57:51 -0700

    3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL and displays the body
+"""
+import sys
+import urllib.request
+import urllib.error
+
+def fetch_url_content(url):
+    try:
+        with urllib.request.urlopen(url) as response:
+
+            content = response.read().decode('utf-8')
+            print(content)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <URL>")
+        sys.exit(1)
+
+    url_to_fetch = sys.argv[1]
+    fetch_url_content(url_to_fetch)
