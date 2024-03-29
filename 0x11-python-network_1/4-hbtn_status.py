03c8c352195aa0475c414363bdaefd5617d283c0
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-03-29 02:52:38 -0700

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-"""script that fetches the url of a website"""
-import request
-req = urllib.request.Request('https://alx-intranet.hbtn.io/status/')
-with urllib.request.urlopen(req) as response:
-    response = response.get(req)
+"""script that fetches the url of a website
+with the use of request function
+"""
+import url.lequest
+url = 'https://alx-intranet.hbtn.io/status'
+response = request.get(url)
+
+print("Body response:")
+print(f"\t- type: {type(response.content)}")
+print(f"\t- content: {response.content.decode('utf-8')}")
