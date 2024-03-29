442f4bd62ccd5f167d1affa46aacd251c498a3b9
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-03-29 02:10:48 -0700

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """script that fetches the url of a website"""
-import urllib.request
-import sys
+import request
 req = urllib.request.Request('https://alx-intranet.hbtn.io/status/')
 with urllib.request.urlopen(req) as response:
-    the_page = response.read()
+    response = response.get(req)
