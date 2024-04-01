db24cea43441ad62a240a4a5ac0eea3a0d756b2e
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-04-01 15:37:20 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 #This  script that takes in a URL, sends a request to that URL,
 #and displays the size of the body of the response
 
-curl -si "$1" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r'
+curl -s "${1}" | wc -c
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -2,21 +2,5 @@
 #This script that takes in a URL, sends a GET request to the URL,
 #and displays the body of the response
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url="$1"
-
-response=$(curl -s -w "%{http_code}" "$url")
-
-status_code="${response: -3}"
-
-if [ "$status_code" -eq 200 ]; then
-    body=$(echo "$response" | sed '$ s/...$//')
-    echo "Response Body:"
-    echo "$body"
-else
-    echo "Error: Non-200 status code received ($status_code)"
-fi
+curl -s -o temp.txt -w "%{http_code}" "$1" && [ $(tail -n1 temp.txt) -eq 200 ] && sed '$d' temp.txt
+rm -f temp.txt
