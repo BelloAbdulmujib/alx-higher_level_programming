a9da4c29731c4efa8c245e16cc275438dcdaa097
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-04-01 17:35:11 -0700

    3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#This script takes in a URL and displays all HTTP methods the server will accept
-
+# This script takes in a URL and displays all HTTP methods the server will accept
 curl -s -I "${1}" | grep "^Allow: .*" | cut -d " " -f 2-
