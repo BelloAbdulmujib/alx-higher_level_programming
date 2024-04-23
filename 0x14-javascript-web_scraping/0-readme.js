2224929946ded89f642b610266b448160586fd36
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-04-23 10:52:37 -0700

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const filesys = require('filesys');
+const filesys = require('fs');
 filesys.readFile(process.arg[v2], 'utf-8',
-  function(error, data) {
-    if (error) {
-      console.log(error);
+  function(err, data) {
+    if (err) {
+      console.log(err);
       return;
     }
     console.log(data);
