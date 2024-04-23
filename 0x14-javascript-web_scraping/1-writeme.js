43506e6a6042296caaa58206917a12b748b112d5
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-04-23 12:43:37 -0700

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const fss = require('fs');
-fs.writeFile(process.arg[v2], write.arg[v3], 'utf-8',
+fs.writeFile(process.arg[v2], process.arg[v3], 'utf-8',
   function(err) {
 	if (err); {
 	console.log(err);
