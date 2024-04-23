d07401c37105a7554db87350a6bf80331e569016
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-04-23 13:06:41 -0700

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+
 fs.writeFile(process.arg[v2], process.arg[v3], 'utf-8',
   function(err) {
 	if (err) {
-	console.log(err);
+	  console.log(err);
 	}
   });
