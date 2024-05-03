648c048e6423a8213762e59f16b1480ed464ec40
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-05-02 14:54:35 -1100

    0x15-javascript-web_jquery

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,3 @@
+// Script script that updates the text color of the header
+
+document.querySelector('head').style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// script that updates the text color of the header element 
+
+document.addEventListener('DOMContentLoaded', function () {
+	document.querySelector('HEADER').style.color = '#FF0000';
+});
